aa01b4c0ee90c3af0c4a6cc81ab36820a4c641eb
juanapisa <jipisa@mail.austral.edu.ar>
2026-03-28 12:52:55 -0300

    Update exercise_calculator.py

--- exercise_calculator.py
+++ exercise_calculator.py
@@ -23,3 +23,22 @@ def calculator():
         Operacion invalida
     """
     pass
+
+    num1 = float(input())
+    num2 = float(input())
+    operacion = input()
+
+    if operacion == "+":
+        print(f"Resultado: {num1+num2}")
+    elif operacion == "*":
+        print(f"Resultado: {num1*num2}")
+    elif operacion == "-":
+        print(f"Resultado: {num1-num2}")
+    elif operacion == "/":
+        if num2 == 0:
+            print("Error: division por cero")
+        else:
+            print(f"Resultado: {num1/num2}")
+    else:
+        print("Operacion invalida")
+
